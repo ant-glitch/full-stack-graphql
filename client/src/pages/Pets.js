77a22d12a560faae3e6b88e8585ea786e44d735d
Antoinette Smith <antoinette@glitch.com>
2019-10-16 09:33:18 -0500

    completed workshop

--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -5,15 +5,76 @@ import PetsList from '../components/PetsList'
 import NewPetModal from '../components/NewPetModal'
 import Loader from '../components/Loader'
 
+const PETS_FIELDS = gql`
+  fragment PetsFields on Pet {
+    id
+      name
+      type
+      img
+      vaccinated @client
+      owner {
+        id
+        age @client
+      }
+  }
+`
+const ALL_PETS = gql`
+  query AllPets {
+    pets {
+      ...PetsFields
+    }
+  }
+  ${PETS_FIELDS}
+`
 
+const ADD_PET = gql`
+  mutation CreatePet($newPet: NewPetInput!) {
+    addPet(input: $newPet) {
+      ...PetsFields
+    }
+  }
+  ${PETS_FIELDS}
+`
 export default function Pets () {
   const [modal, setModal] = useState(false)
-
-
+  const {data, loading, error} = useQuery(ALL_PETS)
+  const [createPet, newPet] = useMutation(ADD_PET, {
+    update(cache, {data: {addPet}}) {
+      const { pets } = cache.readQuery({ query: ALL_PETS })
+      cache.writeQuery({
+        query: ALL_PETS,
+        data: { pets: [addPet].concat(pets)}
+      })
+    }
+  })
   const onSubmit = input => {
     setModal(false)
+    createPet({
+      variables: {newPet: input},
+      optimisticResponse: {
+        __typename: "Mutation",
+        addPet: {
+          id: "temp",
+          __typename: "Pet",
+          name: input.name,
+          type: input.type,
+          img: "https://via.placeholder.com/300",
+          owner: {
+            id: 'none',
+            age: 21
+          },
+          vaccinated: false
+      }}
+    })
   }
-  
+  if (loading) {
+    return <Loader />
+  }
+
+  if (error || newPet.error) {
+    return <p>ERROR</p>
+  }
+  console.log(data.pets[0])
   if (modal) {
     return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />
   }
@@ -32,7 +93,7 @@ export default function Pets () {
         </div>
       </section>
       <section>
-        <PetsList />
+        <PetsList pets={loading ? [] : data.pets}/>
       </section>
     </div>
   )
